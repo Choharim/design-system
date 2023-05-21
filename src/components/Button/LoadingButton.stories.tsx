048063bbb26415e8db692d4039f3d270c80336ca
Choharim <73021666+Choharim@users.noreply.github.com>
2023-05-21 21:12:24 +0900

    feat: loadingButton storybook

--- src/components/Button/LoadingButton.stories.tsx
+++ src/components/Button/LoadingButton.stories.tsx
@@ -1,8 +1,10 @@
 import { StoryObj, Meta } from '@storybook/react'
 import LoadingButton from './LoadingButton'
+import { useEffect, useState } from 'react'
 
 const meta: Meta<typeof LoadingButton> = {
   title: 'components/LoadingButton',
+  tags: ['autodocs'],
   component: LoadingButton,
   args: {
     isLoading: true,
@@ -13,9 +15,25 @@ export default meta
 
 type Story = StoryObj<typeof LoadingButton>
 
+const Template = (args: Story['args']) => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setIsLoading((prev) => !prev)
+    }, 2000)
+
+    return () => {
+      if (id) clearInterval(id)
+    }
+  }, [])
+
+  return <LoadingButton {...args} isLoading={isLoading} />
+}
 export const Basic: Story = {
   args: {
     children: 'loading',
     spinnerSize: 'small',
   },
+  render: Template,
 }
